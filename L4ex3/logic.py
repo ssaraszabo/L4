2013304d6778d548f2bc81a027b204c0a7a1fc5d
ssaraszabo <148943175+ssaraszabo@users.noreply.github.com>
2023-11-24 11:18:27 +0200

    Update logic.py

--- L4ex3/logic.py
+++ L4ex3/logic.py
@@ -1,29 +1,18 @@
 def logic(player, calc):
+    s = 0
     if player == 1:
         if calc == 'stein':
-            return False
+            s = -1
         elif calc == 'papier':
-            return True                 #apar toate combinatiile de alegere, mai putin cele identice
+            s = 1                 #apar toate combinatiile de alegere, mai putin cele identice
     elif player == 2:
         if calc == 'schere':
-            return True
+            s = 1
         elif calc == 'papier':
-            return False
+            s = -1
     elif player == 3:
         if calc == 'schere':
-            return False
+            s = -1
         elif calc == 'stein':
-            return True
-
-def test(player, calc):
-    if player == 1:
-        if calc == 'schere':
-            return False
-    elif player == 2:               #verifica daca calculatorul si jucatorul au ales acelasi lucru
-        if calc == 'stein':
-            return False
-    elif player == 3:
-        if calc == 'papier':
-            return False
-    else:
-        return True
+            s = 1
+    return s
