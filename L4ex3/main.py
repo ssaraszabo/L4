35bebb2d32b77f8913bbf9be159e8c48eb4436c4
ssaraszabo <148943175+ssaraszabo@users.noreply.github.com>
2023-11-24 11:17:36 +0200

    Update main.py

--- L4ex3/main.py
+++ L4ex3/main.py
@@ -11,30 +11,21 @@ def menu():
 
 def main():
     mylist = ['schere', 'stein', 'papier']
-    cont = 0
-    y1 = 0              #punctaj calculator
-    y2 = 0              #punctaj jucator
-    while cont < 3:
+    cont = 3
+    while cont >> 0:        #< 3
         menu()
-        opt = int(input("choose: "))
         x = random.choice(mylist)
+        opt = int(input("choose: "))
         print(x)
-        if test(opt, x):
-            if logic(opt, x):
-                y2 += 1             #returneaza True daca jucatorul castiga
-            else:
-                y1 += 1             #returneaza False --"--
-        cont += 1
+        score += logic(opt, x)
+        cont -= 1
 
-    if y1 >> y2:
-        print("\nYOU LOST!\n")
-        print(y1, ':', y2)
-    elif y1 << y2:
+    if score >> 0:
         print("\nYOU WON!\n")
-        print(y2, ':', y1)
+    elif score << 0:
+        print("\nYOU LOST!\n")
     else:
         print("\nNO WINNER!\n")
-        print(y1, ':', y2)
 
 main()
 
